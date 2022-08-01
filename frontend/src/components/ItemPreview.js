d1602e3b8cfc4f91a431fe38777b5af4d3eb0911
Nalin Singh <nalinsingh20@gmail.com>
2022-08-01 22:33:38 +0530

    Ran Prettier

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -37,7 +37,7 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item"
-        src={item.image?item.image:placeholderImage}
+        src={item.image ? item.image : placeholderImage}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
