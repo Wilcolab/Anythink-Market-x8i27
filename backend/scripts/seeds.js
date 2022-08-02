e14e56ce5d2e851a4de61e74a1cf0460f8815ed2
Nalin Singh <nalinsingh20@gmail.com>
2022-08-03 00:37:36 +0530

    Process Exit

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -10,66 +10,69 @@ const Item = mongoose.model("Item");
 const Comment = mongoose.model("Comment");
 
 if (!process.env.MONGODB_URI) {
-  console.warn("Missing MONGODB_URI in env, please add it to your .env file");
+	console.warn("Missing MONGODB_URI in env, please add it to your .env file");
 }
 
 mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log("\n\nConnection Open\n\n");
-  })
-  .catch((err) => {
-    console.log("\n\n" + err + "\n\n");
-  });
+	.connect(process.env.MONGODB_URI)
+	.then(() => {
+		console.log("\n\nConnection Open\n\n");
+	})
+	.catch((err) => {
+		console.log("\n\n" + err + "\n\n");
+		process.exit();
+	});
 
 const seedDB = async () => {
-  //Creating 100 records
-  for (let itr = 0; itr < 100; itr++) {
-    const seedUser = {
-      username: `user${itr}`,
-      email: `user${itr}@gmail.com`,
-      bio: `SDE ${itr}`,
-      image:
-        "https://i1.wp.com/media.globalnews.ca/videostatic/353/499/oldestmanthumb.jpg?w=1040&quality=70&strip=all",
-    };
+	//Creating 100 records
+	for (let itr = 0; itr < 100; itr++) {
+		const seedUser = {
+			username: `user${itr}`,
+			email: `user${itr}@gmail.com`,
+			bio: `SDE ${itr}`,
+			image:
+				"https://i1.wp.com/media.globalnews.ca/videostatic/353/499/oldestmanthumb.jpg?w=1040&quality=70&strip=all",
+		};
 
-    let userID = null;
-    const user = new User(seedUser);
-    await user.save((err, record) => {
-      userID = record.id;
-    });
+		let userID = null;
+		const user = new User(seedUser);
+		await user.save((err, record) => {
+			userID = record.id;
+		});
 
-    const seedItem = {
-      slug: `Product${itr}-${userID}`,
-      title: `Product${itr}`,
-      description: `User Specialist Product`,
-      image: `https://random.imagecdn.app/360/360`,
-      comments: [],
-      tagList: ["seeding"],
-      seller: userID,
-    };
+		const seedItem = {
+			slug: `Product${itr}-${userID}`,
+			title: `Product${itr}`,
+			description: `User Specialist Product`,
+			image: `https://random.imagecdn.app/360/360`,
+			comments: [],
+			tagList: ["seeding"],
+			seller: userID,
+		};
 
-    let itemID = null;
-    const item = new Item(seedItem);
-    await item.save((err, record) => {
-      itemID = record.id;
-    });
+		let itemID = null;
+		const item = new Item(seedItem);
+		await item.save((err, record) => {
+			itemID = record.id;
+		});
 
-    const seedComment = {
-      body: "Fake Product, Bad Service",
-      seller: userID,
-      item: itemID,
-    };
+		const seedComment = {
+			body: "Fake Product, Bad Service",
+			seller: userID,
+			item: itemID,
+		};
 
-    const comment = new Comment(seedComment);
-    await comment.save();
-  }
+		const comment = new Comment(seedComment);
+		await comment.save();
+	}
 };
 
 seedDB()
-  .then(() => {
-    console.log("\n\n\n\nSeeding Successful");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+	.then(() => {
+		console.log("\n\n\n\nSeeding Successful");
+		process.exit();
+	})
+	.catch((err) => {
+		console.log(err);
+		process.exit();
+	});
