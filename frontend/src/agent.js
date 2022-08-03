2c5ba4c84b9c4a6b2b3e70bf3e6990393ff015fb
Nalin Singh <nalinsingh20@gmail.com>
2022-08-03 14:39:47 +0530

    BACKEND_URL env var

--- frontend/src/agent.js
+++ frontend/src/agent.js
@@ -3,10 +3,12 @@ import _superagent from "superagent";
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
+const BACKEND_URL = process.env.BACKEND_URL;
+
 const API_ROOT =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000/api"
-    : "https://api.anythink.market/api";
+    : BACKEND_URL;
 
 const encode = encodeURIComponent;
 const responseBody = (res) => res.body;
